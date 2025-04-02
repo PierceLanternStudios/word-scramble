d7fcdf31696f9ef557c803283272a2559a524730
PierceLanternStudios <pierceclark07@gmail.com>
2025-04-01 21:48:24 -0400

    implemented first pass of app()

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Statement } from "typescript";
+import { JsxAttribute, Statement } from "typescript";
+import { json } from "stream/consumers";
 
 type State =
   | { phase: "Pre-Game" }
@@ -11,7 +12,9 @@ type Action =
   | { type: "start-game" }
   | { type: "update-guess"; newGuess: string };
 
-function App() {}
+// ######################################################################
+// ==================    State Reducer      =============================
+// ######################################################################
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
@@ -28,9 +31,75 @@ function reducer(state: State, action: Action): State {
   }
   return state;
 }
+// ######################################################################
+// ==================     Utilities      ================================
+// ######################################################################
 
 function getRandomWord(): string {
-  return "llama";
+  const RANDOMWORDS = [
+    "llama",
+    "tiger",
+    "wildebeest",
+    "aardvark",
+    "puma",
+    "spotted-turtle",
+  ];
+  return RANDOMWORDS[Math.floor(Math.random() * RANDOMWORDS.length)];
+}
+
+function getInitialState(): State {
+  return { phase: "Pre-Game" };
+}
+
+// ######################################################################
+// ==================     App Render     ================================
+// ######################################################################
+
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, null, getInitialState);
+
+  switch (state.phase) {
+    case "Pre-Game":
+      return (
+        <div>
+          <h3>Pre Game!</h3>
+          <button onClick={() => dispatch({ type: "start-game" })}>
+            Start Game!
+          </button>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
+      );
+
+    case "In-Game":
+      return (
+        <div>
+          <h3>In Game!</h3>
+          <div>Goal: {state.goal}</div>
+          <label>
+            Guess:{" "}
+            <input
+              type="text"
+              value={state.guess}
+              onChange={(ev) =>
+                dispatch({ type: "update-guess", newGuess: ev.target.value })
+              }
+            />
+          </label>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
+      );
+    case "Post-Game":
+      return (
+        <div>
+          <h3>Nice Job!</h3>
+          <div>You guessed "{state.goal}"!</div>
+          <button onClick={() => dispatch({ type: "start-game" })}>
+            Start a new Game!
+          </button>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
+      );
+  }
 }
 
 export default App;
