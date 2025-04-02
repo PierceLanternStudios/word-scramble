857a404352438f4dcda0f199a7a312c3599f9031
PierceLanternStudios <pierceclark07@gmail.com>
2025-04-01 21:18:02 -0400

    defined state and action types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import { Statement } from "typescript";
 
-function App() {
-  return <div>Hello World!</div>;
-}
+type State =
+  | { phase: "Pre-Game" }
+  | { phase: "In-Game"; goal: string; guess: string }
+  | { phase: "Post-Game"; goal: string };
+
+type Action =
+  | { type: "start-game" }
+  | { type: "update-guess"; newGuess: string };
+
+function App() {}
+
+function reducer(state: State, action: Action) {}
 
 export default App;
